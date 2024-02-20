47fad5a00a3145441c6d498a3d9cc07ca951f2be
Exitare <business.exitare@icloud.com>
2024-02-19 18:33:48 -0800

    Update job_scheduler.sh

--- code/job_scheduler.sh
+++ code/job_scheduler.sh
@@ -7,7 +7,7 @@
 # create a list of files
 files=(pancreatic-adeno+lung-adeno_transcriptomics_cell-line+CPTAC.tsv renal-clear-cell+colon-adeno_transcriptomics_cell-line+CPTAC.tsv renal-clear-cell+glioblastoma_transcriptomics_cell-line+CPTAC.tsv renal-clear-cell+lung-adeno_transcriptomics_cell-line+CPTAC.tsv renal-clear-cell+pancreatic-adeno_transcriptomics_cell-line+CPTAC.tsv)
 cancer_weights=(2 2.5 3 3.5 4)
-system_weights=(2 2.5 3 3.5 4)
+systems_weights=(2 2.5 3 3.5 4)
 
 
 # iterate thorugh files
@@ -15,8 +15,8 @@ for file in "${files[@]}"; do
   # iterate thorugh the pair wise combination of cancer and system weights
   for i in $(seq 0 4); do
     for j in $(seq 0 4); do
-      echo file="${file}" cancer_weight="${cancer_weights[i]}" system_weight="${system_weights[j]}"
-      sbatch ./schedule_vae_cpu.sh 50 64 1500 ../output/cancer_pairs/${file}  ../results/ --cancer_weight "${cancer_weights[i]}" --system_weight "${system_weights[j]}"
+      echo file="${file}" cancer_weight="${cancer_weights[i]}" system_weight="${systems_weights[j]}"
+      sbatch ./schedule_vae_cpu.sh 50 64 1500 ../output/cancer_pairs/${file}  ../results/ "${cancer_weights[i]}" "${systems_weights[j]}"
     done
   done
 done
