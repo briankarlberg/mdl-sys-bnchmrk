f0ddd5f2b81b25ff0bde947f3ebed7fd2feb8720
Exitare <business.exitare@icloud.com>
2024-02-21 11:06:15 -0800

    Fix v1 scripts

--- code/schedule_lsp_v1.sh
+++ code/schedule_lsp_v1.sh
@@ -7,6 +7,6 @@ files=(pancreatic-adeno+lung-adeno_transcriptomics_cell-line+CPTAC.tsv renal-cle
 # iterate thorugh files
 for file in "${files[@]}"; do
   # iterate thorugh the pair wise combination of cancer and system weights
-    echo file="${file}" cancer_weight="${cancer_weights[i]}" system_weight="${systems_weights[j]}"
-    sbatch ./schedule_vae_cpu.sh 50 64 1500 ../output/cancer_pairs/${file}  ../results/ "${cancer_weights[i]}" "${systems_weights[j]}"
+    echo file="${file}"
+    sbatch ./schedule_vae_cpu.sh 50 64 1500 ../output/cancer_pairs/${file}
 done
