d280390aa4913edb068eed7ed25b94128d3fa844
Exitare <business.exitare@icloud.com>
2024-03-21 10:15:45 -0700

    Update vaes

--- code/multi_cancer_vae.py
+++ code/multi_cancer_vae.py
@@ -6,74 +6,17 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.layers import Input, Dense, Layer
 from tensorflow.keras.models import Model
-import matplotlib.pyplot as plt
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
-from tensorflow.keras.losses import MeanSquaredError
 
 systems_column = "System"
 cancer_column = "Cancer_type"
 
 latent_space_dim = 50
-batch_size = 64
-epochs = 100
-
-
-def contrastive_loss(z1, z2, labels, margin=1.0):
-    """
-    Compute the contrastive loss given pairs of latent representations and a binary label indicating
-    whether the pair is from the same class or not.
-
-    Parameters:
-    - z1, z2: The latent representations of pairs of samples (output of the encoder).
-    - labels: The binary labels indicating whether the pair is similar (1) or dissimilar (0).
-    - margin: The margin enforced between dissimilar pairs.
-
-    Returns:
-    - A scalar contrastive loss value.
-    """
-    # Calculate the euclidean distance between the latent representations
-    distances = tf.reduce_sum(tf.square(z1 - z2), axis=1)
-
-    # Cast labels to float32 to match distances data type
-    labels = tf.cast(labels, dtype=tf.float32)
-
-    # Calculate the contrastive loss
-    similar_loss = labels * distances
-    dissimilar_loss = (1 - labels) * tf.maximum(margin - tf.sqrt(distances), 0.0)
-    contrastive_loss_value = 0.5 * tf.reduce_mean(similar_loss + dissimilar_loss)
-
-    return contrastive_loss_value
-
-
-def make_pairs(features, labels):
-    pair_features = []
-    pair_labels = []
-
-    # Indices of samples for each class
-    class_indices = {label: np.where(labels == label)[0] for label in np.unique(labels)}
-
-    # Convert features to a NumPy array if it's a pandas DataFrame
-    if isinstance(features, pd.DataFrame):
-        features = features.to_numpy()
-
-    # Iterate over class_indices to create pairs
-    for class_label, indices in class_indices.items():
-        # Positive pairs from the same class
-        for i in range(len(indices)):
-            for j in range(i + 1, len(indices)):
-                pair_features.append([features[indices[i]], features[indices[j]]])
-                pair_labels.append(1)
-
-        # Negative pairs from different classes
-        all_other_indices = np.hstack([idxs for key, idxs in class_indices.items() if key != class_label])
-        for i in indices:
-            for j in all_other_indices:
-                pair_features.append([features[i], features[j]])
-                pair_labels.append(0)
-
-    return np.array(pair_features), np.array(pair_labels)
+batch_size = 16
 
 
 class Sampling(Layer):
@@ -108,19 +51,45 @@ def create_decoder():
     return model
 
 
+# Discriminator model for systems prediction
+def create_discriminator():
+    latent_inputs = layers.Input(shape=(latent_space_dim,))
+    x = layers.Dense(input_dim // 2, activation='relu')(latent_inputs)
+    outputs = layers.Dense(1, activation='sigmoid')(x)
+    model = models.Model(latent_inputs, outputs, name='discriminator')
+    return model
+
+
+def build_cancer_classifier(latent_dim, num_classes):
+    # Define the classifier model
+    classifier_input = layers.Input(shape=(latent_dim,))
+    x = layers.Dense(latent_dim // 2, activation='relu')(classifier_input)
+    x = layers.Dropout(0.2)(x)  # Dropout layer for regularization
+    x = layers.Dense(latent_dim // 3, activation='relu')(x)
+    x = layers.Dropout(0.2)(x)  # Dropout layer for regularization
+    x = layers.Dense(latent_dim // 4, activation='relu')(x)
+    classifier_output = layers.Dense(num_classes, activation='softmax')(x)
+
+    # Create a Keras model
+    classifier_model = models.Model(classifier_input, classifier_output, name='classifier')
+
+    return classifier_model
+
+
 class VAE(models.Model):
-    def __init__(self, encoder, decoder, **kwargs):
+    def __init__(self, encoder, decoder, discriminator, classifier, **kwargs):
         super(VAE, self).__init__(**kwargs)
         self.encoder = encoder
         self.decoder = decoder
-
+        self.discriminator = discriminator
+        self.classifier = classifier
         # Initialize the metrics
         self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
         self.vae_loss_tracker = tf.keras.metrics.Mean(name="vae_loss")
         self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name="reconstruction_loss")
         self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
-
-        self.contrastive_loss_tracker = tf.keras.metrics.Mean(name="contrastive_loss")
+        self.discriminator_loss_tracker = tf.keras.metrics.Mean(name="discriminator_loss")
+        self.classification_loss_tracker = tf.keras.metrics.Mean(name="classification_loss")
         self.vae_optimizer = None
         self.disc_optimizer = None
 
@@ -133,42 +102,47 @@ class VAE(models.Model):
         # `reset_states()` yourself at the time of your choosing.
         return [
             self.total_loss_tracker,
-            self.vae_loss_tracker,
             self.reconstruction_loss_tracker,
             self.kl_loss_tracker,
-            self.contrastive_loss_tracker
+            self.discriminator_loss_tracker,
         ]
 
-    def compile(self, vae_optimizer, **kwargs):
+    def compile(self, vae_optimizer, disc_optimizer, **kwargs):
         super().compile(**kwargs)
         self.vae_optimizer = vae_optimizer
+        self.disc_optimizer = disc_optimizer
 
     def train_step(self, data):
-        features1 = data[0][:, 0]
-        features2 = data[0][:, 1]
-        labels = data[1]
-        with (tf.GradientTape() as vae_tape, tf.GradientTape() as disc_tape):
-            z_mean1, z_log_var1, z1 = self.encoder(features1)
-            z_mean2, z_log_var2, z2 = self.encoder(features2)
-            reconstruction1 = self.decoder(z1)
-            reconstruction2 = self.decoder(z2)
+        features, cancer_train_data, systems_train_data = data
+        with tf.GradientTape() as vae_tape, tf.GradientTape() as disc_tape:
+            z_mean, z_log_var, z = self.encoder(features)
+            x_reconstruction = self.decoder(z)
 
-            # Calculate VAE loss components for each feature in the pair
-            reconstruction_loss = MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE)(features1,
-                                                                                                            reconstruction1) + \
-                                  MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE)(features2,
-                                                                                                            reconstruction2)
+            # VAE loss
+            reconstruction_loss = mean_squared_error(features, x_reconstruction)
+            kl_loss = - 0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=-1)
+            vae_loss = tf.reduce_mean(reconstruction_loss + kl_loss)
 
-            kl_loss = -0.5 * (1 + z_log_var1 - tf.square(z_mean1) - tf.exp(z_log_var1)) \
-                      - 0.5 * (1 + z_log_var2 - tf.square(z_mean2) - tf.exp(z_log_var2))
-            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            # Forward pass through the classifier using latent representation
+            cancer_pred = self.classifier(z)
 
-            # Compute contrastive loss using the latent representations and labels
-            c_loss = contrastive_loss(z1, z2, labels, 1)
-            # Total VAE loss: reconstruction + KL divergence + contrastive loss
-            vae_loss = reconstruction_loss + kl_loss
+            # Compute classification loss
+            # classification_loss = tf.keras.losses.categorical_crossentropy(cancer_data, cancer_pred)
+            classification_loss = 3 * tf.reduce_mean(
+                tf.keras.losses.categorical_crossentropy(cancer_train_data, cancer_pred, from_logits=False))
 
-            total_loss = vae_loss + c_loss
+            # Discriminator loss
+            system_pred = self.discriminator(z)
+            discriminator_loss = 0.5 * BinaryCrossentropy(from_logits=False)(tf.zeros_like(system_pred), system_pred)
+
+            # Adjusted VAE loss
+            total_loss = vae_loss - discriminator_loss + classification_loss
+
+            # vae_loss = vae_loss + tf.reduce_mean(classification_loss) - tf.reduce_mean(discriminator_loss)
+
+        # Train discriminator to be unable to distinguish
+        gradients_disc = disc_tape.gradient(discriminator_loss, self.discriminator.trainable_weights)
+        self.disc_optimizer.apply_gradients(zip(gradients_disc, self.discriminator.trainable_weights))
 
         gradients_vae = vae_tape.gradient(total_loss, self.encoder.trainable_weights + self.decoder.trainable_weights)
         self.vae_optimizer.apply_gradients(
@@ -179,14 +153,16 @@ class VAE(models.Model):
         self.vae_loss_tracker.update_state(vae_loss)
         self.reconstruction_loss_tracker.update_state(reconstruction_loss)
         self.kl_loss_tracker.update_state(kl_loss)
-        self.contrastive_loss_tracker.update_state(c_loss)
+        self.discriminator_loss_tracker.update_state(discriminator_loss)
+        self.classification_loss_tracker.update_state(classification_loss)
 
         return {
             "total_loss": total_loss,
             "vae_loss": vae_loss,
             "reconstruction_loss": reconstruction_loss,
             "kl_loss": kl_loss,
-            "contrastive_loss": c_loss
+            "discriminator_loss": discriminator_loss,
+            "classification_loss": classification_loss
         }
 
 
@@ -199,15 +175,22 @@ if __name__ == '__main__':
                         help="The output dir where the results should be stored. If it does not exist, it will be created.")
     args = parser.parse_args()
 
-
-    file_name = "_".join(Path(args.data).stem.split('_')[:-1])
-    system_1_name = Path(args.data).stem.split('+')[0].split('_')[-1]
-    system_2_name = Path(args.data).stem.split('+')[-1]
     loaded_data = pd.read_csv(args.data, sep="\t", index_col=0)
+    loaded_data.reset_index(drop=True, inplace=True)
 
-    output_dir = Path(args.output_dir, file_name)
+    output_dir = args.output_dir
     if not output_dir.exists():
         output_dir.mkdir(parents=True, exist_ok=True)
+
+    # original_output_dir = Path(output_dir, Path(data_folder).stem)
+    # data_specific_output_dir = original_output_dir
+    # for i in range(10000):  # Assuming a sensible upper limit to avoid infinite loops
+    #    if not data_specific_output_dir.exists():
+    #        break
+    #    data_specific_output_dir = Path(f"{data_specific_output_dir}_{i + 1}")
+
+    # if not data_specific_output_dir.exists():
+    #    data_specific_output_dir.mkdir(parents=True, exist_ok=True)
 
     print("Detecting systems...")
     systems = loaded_data[systems_column].unique()
@@ -222,9 +205,6 @@ if __name__ == '__main__':
     # select the data
     s1_data = loaded_data[loaded_data[systems_column] == systems[0]]
     s2_data = loaded_data[loaded_data[systems_column] == systems[1]]
-
-    s1_sample_ids = s1_data.index
-    s2_sample_ids = s2_data.index
 
     # detect cancer types
     cancer_types = loaded_data[cancer_column].unique()
@@ -281,19 +261,18 @@ if __name__ == '__main__':
 
     encoder = create_encoder()
     decoder = create_decoder()
+    discriminator = create_discriminator()
+    cancer_classifier = build_cancer_classifier(latent_dim=latent_space_dim, num_classes=len(np.unique(s1_cancer_enc)))
 
-    paired_features, paired_labels = make_pairs(s1_data, s1_cancer_series_enc)
-
-    # Convert to TensorFlow dataset
-    train_dataset = tf.data.Dataset.from_tensor_slices((paired_features, paired_labels))
-    # Shuffle and batch the dataset
-    train_dataset = train_dataset.shuffle(buffer_size=len(paired_labels)).batch(batch_size)
+    train_dataset = tf.data.Dataset.from_tensor_slices((s1_data, s1_cancer_enc, s1_system_enc))
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
 
     # add early stopping
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor='total_loss', patience=5)
 
-    vae = VAE(encoder, decoder)
-    vae.compile(vae_optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.0001))
+    vae = VAE(encoder, decoder, discriminator, cancer_classifier)
+    vae.compile(vae_optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.0001),
+                disc_optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.0001))
 
     history = vae.fit(train_dataset, epochs=100, batch_size=batch_size, callbacks=[early_stopping])
 
@@ -303,9 +282,6 @@ if __name__ == '__main__':
     combined_system_enc = pd.concat([s1_system_enc, s2_system_enc], axis=0)
     combined_cancer_series_enc = pd.concat([s1_cancer_series_enc, s2_cancer_series_enc], axis=0)
     combined_system_series_enc = pd.concat([s1_system_series_enc, s2_system_series_enc], axis=0)
-    combined_cancers = pd.Series(pd.concat([s1_cancer_data, s2_cancer_data], axis=0))
-    combined_systems = pd.Series(pd.concat([s1_system_data, s2_system_data], axis=0))
-    combined_sample_ids = pd.concat([pd.Series(s1_sample_ids), pd.Series(s2_sample_ids)], axis=0)
 
     cancer_data = loaded_data[cancer_column]
     system_data = loaded_data[systems_column]
@@ -315,33 +291,18 @@ if __name__ == '__main__':
 
     # create a dataframe with the latent space
     z_df = pd.DataFrame(latent_space)
-    umap_df = z_df.copy()
-    # save latent space
-    z_df.insert(0, cancer_column, combined_cancers.values)
-    z_df.insert(0, systems_column, combined_systems.values)
-    z_df.index = combined_sample_ids.values
 
-    # create latent space file name
-    file_name = f"{file_name}_{system_1_name}+{system_2_name}.cntrstv-loss_{latent_space_dim}-ltnt-dim_{epochs}-epchs"
-    latent_space_file_name = f"{file_name}.tsv"
-    print(f"Saving latent space using file name: {latent_space_file_name}")
-    z_df.to_csv(Path(output_dir, latent_space_file_name), index=True, sep="\t")
+    # create umap
+    import umap
 
-    # plot the training history
-    plt.plot(history.history['total_loss'])
-    plt.plot(history.history['vae_loss'])
-    plt.plot(history.history['reconstruction_loss'])
-    plt.plot(history.history['kl_loss'])
-    plt.plot(history.history['contrastive_loss'])
-    plt.title('Contrastive VAE loss')
+    reducer = umap.UMAP()
+    embedding = reducer.fit_transform(z_df)
 
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
+    # plot the umap
+    import matplotlib.pyplot as plt
+    import seaborn as sns
 
-    # log the y axis
-    plt.yscale('log')
-
-    plt.legend(['Total loss', 'VAE loss', 'Reconstruction loss', 'KL loss', 'Contrastive loss'], loc='upper right')
-    # save fig
-    image_file_name = f"{file_name}_loss_history.png"
-    plt.savefig(Path(output_dir, image_file_name), dpi=300)
+    sns.scatterplot(x=embedding[:, 0], y=embedding[:, 1], hue=cancer_data, palette="viridis", style=system_data)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.tight_layout()
+    plt.show()
